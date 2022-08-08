b06025155d7d5628077707fed615c46f6fcf35b0
Marcel MBP <25566817+cestmarcel@users.noreply.github.com>
2022-08-08 06:03:23 -0400

    Added animation to headshot

--- scripts.js
+++ scripts.js
@@ -51,4 +51,17 @@ $(document).ready(function() {
       $(window).on('scroll', checkInView);
     }
   
+    const observer = new IntersectionObserver(entries => {
+      // Loop over the entries
+      entries.forEach(entry => {
+        // If the element is visible
+        if (entry.isIntersecting) {
+          // Add the animation class
+          entry.target.classList.add('animated-image-animation');
+        }
+      });
+    });
+    
+    observer.observe(document.querySelector('.animated-image'));
+
   });
