3817d94522b740cf9253d62e8f159ce868c6afd4
Marcel MBP <25566817+cestmarcel@users.noreply.github.com>
2022-08-10 04:02:07 -0400

    Wrote README and cleaned up scripts.js

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# start
+# Psychotherapy website
+
+## Description
+
+This is the source code for a psychotherapy website. It was created using HTML/CSS, Bootstrap, and JavaScript.
+
+## Table of contents
+
+- Usage
+- License
+- Credits
+
+## Usage
+
+The application is intended to serve as a digital portfolio for a psychotherapy practice. The page is available in English. It has the following features:
+- Therapist bio
+- Practice overview
+- Signup process overview
+- FAQ section
+- Contact form
+
+## License
+
+No license
+
+## Credits
+
+- Bootstrap 5 was used to facilitate mobile-first development. Documentation is available under: [https://getbootstrap.com/docs/4.1/getting-started/introduction/](https://getbootstrap.com/docs/4.1/getting-started/introduction/)
+- Google Fonts was used for the web fonts. More information: [https://fonts.google.com/about](https://fonts.google.com/about)
+- Formspree.io was used to create a contact form with ReCaptcha functionality. Documentation here: [https://help.formspree.io/hc/en-us](https://help.formspree.io/hc/en-us)
+- Vimeo is embedded to create the responsive video header of the website. Documentation available here: [https://developer.vimeo.com/api/reference](https://developer.vimeo.com/api/reference)
--- scripts.js
+++ scripts.js
@@ -11,46 +11,6 @@ $(document).ready(function() {
   
     });
   
-    $(window).scroll(function(){
-      var scroll_position = $(window).scrollTop()/2;
-  
-      if (window.innerWidth < 785) {
-        scroll_position = scroll_position / 4;
-      }
-  
-      $('.header-parallax').css({
-          'background-position-y' : - scroll_position + "px",
-      })
-    
-    })
-  
-    var $animation_elements = $('.animate__animated');
-  
-    function checkInView() {
-      var window_height = $(window).height();
-      var window_top_position = $(window).scrollTop();
-      var window_bottom_position = (window_top_position + window_height);
-    
-      $.each($animation_elements, function() {
-        var $element = $(this);
-        var element_height = $element.outerHeight();
-        var element_top_position = $element.offset().top;
-        var element_bottom_position = (element_top_position + element_height);
-    
-        //check to see if this current container is within viewport
-        if ((element_bottom_position >= window_top_position) &&
-            (element_top_position <= window_bottom_position)) {
-          $element.addClass($element.data("animation"));
-        } else {
-          $element.removeClass($element.data("animation"));
-        }
-      });
-    }
-  
-    if (window.innerWidth > 785) {
-      $(window).on('scroll', checkInView);
-    }
-  
     const observer = new IntersectionObserver(entries => {
       // Loop over the entries
       entries.forEach(entry => {
